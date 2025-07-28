88c74abee3ecd7543a559d81f5ec40af7aa6c98f
Serork <mandrygin.2013@mail.ru>
2025-07-29 02:53:59 +0300

    mod calls (1)

---
+++ CustomLiquidsModCalls.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Terraria;
+
+namespace RoALiquids;
+
+static class CustomLiquidsModCalls {
+    public static object Call(params object[] args) {
+        RoALiquids riseOfAgesLiquids = RoALiquids.Instance;
+        Array.Resize(ref args, 2);
+        string success = "Success";
+        try {
+            string message = args[0] as string;
+            if (message == "IsTarWet") {
+                if (args[1] is not NPC npc) {
+                    throw new Exception($"{args[1]} is not NPC");
+                }
+                return npc.GetGlobalNPC<CustomLiquidCollision_NPC>().tarWet;
+            }
+        }
+        catch (Exception e) {
+            riseOfAgesLiquids.Logger.Error($"Call Error: {e.StackTrace} {e.Message}");
+        }
+        return null;
+    }
+}
--- RoALiquids.cs
+++ RoALiquids.cs
@@ -11,6 +11,14 @@ namespace RoALiquids;
 sealed class RoALiquids : Mod {
     public static string LiquidTexturesPath => $"{nameof(RoALiquids)}/Resources/Liquids/";
 
+    private static RoALiquids? _instance;
+
+    public RoALiquids() {
+        _instance = this;
+    }
+
+    public static RoALiquids Instance => _instance ??= ModContent.GetInstance<RoALiquids>();
+
     public override IContentSource CreateDefaultContentSource() => new CustomContentSource(base.CreateDefaultContentSource());
 
     public override void PostSetupContent() {
@@ -22,4 +30,6 @@ sealed class RoALiquids : Mod {
     public override void HandlePacket(BinaryReader reader, int sender) {
         MultiplayerSystem.HandlePacket(reader, sender);
     }
+
+    public override object Call(params object[] args) => CustomLiquidsModCalls.Call(args);
 }
